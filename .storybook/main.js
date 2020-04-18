2c3769c61054662df270fc5eb513b717c22866af
draintheshallows <kli@quilhealth.com>
2020-04-18 14:31:11 -0400

    knobs addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,5 +2,5 @@
 
 module.exports = {
   stories: ['../src/components/**/*.stories.js'],
-  addons: ['@storybook/preset-create-react-app','@storybook/addon-actions', '@storybook/addon-links'],
+  addons: ['@storybook/preset-create-react-app','@storybook/addon-actions', '@storybook/addon-knobs', '@storybook/addon-links'],
 };
